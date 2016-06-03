6095175168b32f750ab9fb2502f7f355a8c4d036
DrobenyukA <Drobenyuk.A@gmail.com>
2016-06-03 20:31:07 +0300

    some data

--- Lesson_26/main.js
+++ Lesson_26/main.js
@@ -1,3 +1,20 @@
-/**
- * Created by drobenyuk on 03.06.16.
- */
+function is_array(instance){
+    if (typeof(instance) === typeof([])){
+        return true;
+    }
+    return false;
+}
+
+function testArray(){
+    var test = [1, 'a', {}, [1, 's']],
+        result = [];
+    for (var i = 0; i < test.length; i++){
+        result.push({
+            type: typeof(test[i]),
+            result: is_array(test[i])
+        });
+    }
+    return result;
+}
+
+console.log(testArray());
